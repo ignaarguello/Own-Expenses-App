a7c8abcafae721fb128640164fcb2ceff0ee4d28
ignaarguello <igna.arguello2110@hotmail.com>
2023-12-14 02:43:14 -0300

    Backgrounds en home y primer titulo de own expenses

--- src/components/BackgroundsHome/BackgroundsHome.jsx
+++ src/components/BackgroundsHome/BackgroundsHome.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import './BackgroundsHome.css'
+import { useEffect, useState } from 'react'
+import fondo_home_1 from '../../assets/images/fondo_home_1.jpeg'
+import fondo_home_2 from '../../assets/images/fondo_home_2.jpeg'
+import fondo_home_3 from '../../assets/images/fondo_home_3.jpeg'
 
 export default function BackgroundsHome() {
+    const images_background = [fondo_home_2, fondo_home_3, fondo_home_1];
+    let [indexImage, setIndexImage] = useState(0);
+
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setIndexImage((prevState) => (prevState + 1) % images_background.length);
+        }, 10000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+
+    const renderImage = (index) => (
+        <img
+            id="image_background_active"
+            src={images_background[index]}
+            alt=""
+            className="blur-image animation:blur-image 1s ease-in-out"
+        />
+    );
+
     return (
-        <div>BackgroundsHome</div>
+        <div id='containerPrincipal-BackgroundsHome'>
+            {renderImage(indexImage)}
+            <h2 id='tituloOwnExpenses'>Own Expenses</h2>
+        </div>
     )
 }
