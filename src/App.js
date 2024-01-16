647945d71d8da1c7490a6e0f3b9afb6bdf711be8
ignaarguello <igna.arguello2110@hotmail.com>
2024-01-16 17:03:35 -0300

    Carousel de background en app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import TituloExpenses from './components/TituloExpenses/TituloExpenses';
 import GridMenuHome from './components/GridMenuHome/GridMenuHome';
 import Counter from './components/Counter/Counter';
 import MenuFooter from './components/MenuFooter/MenuFooter';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { images_array } from './data/images_background_data';
 
 
 function App() {
 
+  /* Hook encargado de crear las propiedades en LS en caso de no existir */
   useEffect(() => {
     let movimientos_storage = localStorage.getItem('movimientos')
     let dinero_total_storage = localStorage.getItem('dinero_total')
@@ -20,8 +22,24 @@ function App() {
     }
   }, [])
 
+  /* Hook encargado del cambio del background */
+  const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setContador((prevContador) => (prevContador + 1) % (images_array.length - 1 + 1));
+    }, 9000);
+    return () => clearInterval(intervalId);
+  }, [contador, images_array]);
+
+
+  /* JSX del componente */
   return (
-    <div className="App">
+    <div className="App" style={
+      {
+        backgroundImage: `url(${images_array[contador]})`,
+        backgroundSize: "cover",
+      }}>
       <TituloExpenses />
       <GridMenuHome />
       <Counter />
---
+++ src/data/images_background_data.js
@@ -0,0 +1,5 @@
+export const images_array = [
+    'https://marketplace.canva.com/_tmqY/MAELxp_tmqY/1/s2/canva-summer-beach-with-palms-trees-MAELxp_tmqY.jpg',
+    'https://marketplace.canva.com/GD38U/MAEgNxGD38U/1/s2/canva-modern-living-room-design-MAEgNxGD38U.jpg',
+    'https://marketplace.canva.com/5ZfWw/MADyRC5ZfWw/1/s2/canva-person-holding-corona-cerveza-bottle-MADyRC5ZfWw.jpg',
+]
